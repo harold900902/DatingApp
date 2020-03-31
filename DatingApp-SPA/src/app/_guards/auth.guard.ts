d77d51dd3a3044b617534d7ff1c6bbb5a8162ef8
harold900902 <garciaharold24@gmail.com>
2020-03-31 11:11:16 -0500

    End of Section 7

--- DatingApp-SPA/src/app/_guards/auth.guard.ts
+++ DatingApp-SPA/src/app/_guards/auth.guard.ts
@@ -18,7 +18,7 @@ export class AuthGuard implements CanActivate {
       return true;
     }
     this.alertify.error('Login first !!!!');
-    this.router.navigate(['/home']);
+    this.router.navigate(['']);
     return false;
   }
 }
